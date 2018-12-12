f97b14dbf1af19f42230f6607a2d4c0fda8f5180
nferraro <ni.ferraro@gmail.com>
2018-12-13 00:51:36 +0100

    temp website

--- run.sh
+++ run.sh
@@ -2,5 +2,14 @@
 
 set -e
 
+# Erase content
+rm -rf docs
+
+# Build content
 ./node_modules/@antora/cli/bin/antora site.yml --stacktrace --pull
+
+# Tell github not to ignore underscores in file names
+echo "" > docs/.nojekyll
+
+# Serve the content
 serve docs
